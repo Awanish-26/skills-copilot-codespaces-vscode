326bac5278bf84469ea7faf3a8a6ad21008a99d9
Awanish <102904502+Awanish-26@users.noreply.github.com>
2024-12-23 06:00:52 +0000

    Copilot third commmit

--- comments.js
+++ comments.js
@@ -1,2 +1,22 @@
 // create web server
-// create a route for comments
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var path = require('path');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/comments');
+var Comment = require('./models/comment');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/comments', function(req, res) {
+  Comment.find(function(err, comments) {
+    if (err) {
+      return res.send(err);
+    }
+    res.json(comments);
+  });
+});
