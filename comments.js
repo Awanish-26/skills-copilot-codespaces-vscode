f3f22cbdfff437a094df1b1b800eb131a81ec2fc
Awanish <102904502+Awanish-26@users.noreply.github.com>
2024-12-23 05:56:57 +0000

    "Copilot third commit"

--- comments.js
+++ comments.js
@@ -1,23 +1,2 @@
 // create web server
-var express = require('express');
-var app = express();
-
-// create comments array
-var comments = [];
-
-// GET /comments
-app.get('/comments', function(req, res) {
-    res.json(comments);
-});
-
-// POST /comments
-app.post('/comments', function(req, res) {
-    var comment = req.body;
-    comments.push(comment);
-    res.json(comment);
-});
-
-// start web server
-app.listen(3000, function() {
-    console.log('Server is listening on port 3000');
-});
+// create a route for comments
